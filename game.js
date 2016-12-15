3bb4d980f28a442fa22eb3586369f4238ef37266
Louise Clark <lclark070607@gmail.com>
2016-12-15 00:22:26 -0500

    ajax file

--- game.js
+++ game.js
@@ -1,26 +1,38 @@
 function init() {
-    let button = document.querySelector('#question');
-    button.addEventListener('click', getNewQuestion);
+    let p = document.querySelector('#question');
+    p.addEventListener('click', getNewQuestion);
 }
+
 console.log('yippee');
+
+
+//My Ajax request to get a new question.
 
 function getNewQuestion() {
     let request = new XMLHttpRequest();
 
-    request.open('GET', 'http://jservice.io/api/random');
+    request.open('GET', "http://jservice.io/api/random");
 
     request.addEventListener('load', function () {
         console.log('weve got mail');
-
-    let response = JSON.parse(request.responseText); 
-    let user = response.results[0];
-
-    console.log(request.responseText);
-    });
-
-    
-    
+    })
 
 
+        let response = JSON.parse(request.responseText);
+        // for (let i=0; i < response.results.length; i++) {
+        //     let trivia = response.results[0];
+            
+            console.log(request.responseText);
+            // showQuestion(trivia);
+        }
+
+request.send();
+
+window.addEventListener('load', init);
 
 
+
+
+
+
+       
